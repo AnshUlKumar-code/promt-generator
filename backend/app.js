010f7632099b53d5cb6d63aa747869fc26e30bb9
Anshul Kumar <anshulkumar73610@gmail.com>
2026-02-19 22:35:47 +0530

    last commit

--- backend/app.js
+++ backend/app.js
@@ -35,7 +35,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-// ❌ REMOVE THIS ENTIRELY:
+
 // const PORT = process.env.PORT || 5000;
 // app.listen(PORT, () => {
 //   console.log(`Server running on port ${PORT}`);
