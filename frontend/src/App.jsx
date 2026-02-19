f88f8411bcc69c48cf450e2a5df93579bdaab43c
Anshul Kumar <anshulkumar73610@gmail.com>
2026-02-19 21:35:35 +0530

    latest commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
       formData.append("image", image);
 
       const response = await axios.post(
-        "http://localhost:5000/api/posts",
+        "https://promt-generator-six.vercel.app/api/posts",
         formData,
         {
           headers: {
