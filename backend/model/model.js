ae4afde6cd552db5b602e0dc6cf14c1b200edf66
Anshul Kumar <anshulkumar73610@gmail.com>
2026-04-23 20:10:36 +0530

    lol

--- backend/model/model.js
+++ backend/model/model.js
@@ -30,8 +30,8 @@ const postSchema = new mongoose.Schema(
 
 // If you want to explicitly define createdAt (though timestamps already does it)
 // You can also add an index for better query performance
-postSchema.index({ category_id: 1 });
-postSchema.index({ tags: 1 });
+// postSchema.index({ category_id: 1 });
+// postSchema.index({ tags: 1 });
 
 const PostModel = mongoose.model("Post", postSchema);
 
